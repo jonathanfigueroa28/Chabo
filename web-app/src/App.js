0e056670295eca915c6c384f038c89ee49e641f7
jonathanfigueroa28 <jonathan.figueroa.h@uni.pe>
2023-11-20 15:14:12 -0500

    importando chatscope

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import '@chatscope/chat-ui-kit-react-styles/dist/default/styles.min.css';
+import {MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator} from '@chatscope/chat-ui-kit-react';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        
+        </div>
     </div>
   );
 }
